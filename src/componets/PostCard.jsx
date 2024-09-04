aee89b7e1a46cdafc3d4350e9539a31b434d0d76
goranghingorani <goranghingorani0219@gmail.com>
2024-09-04 11:56:54 +0530

    some more changes for the morning

--- src/componets/PostCard.jsx
+++ src/componets/PostCard.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import service from "../appwrite/config";
 import { Link } from "react-router-dom";
+import "../App.css";
 
-function PostCard({$id,title,featuredimage}){
-return(
+function PostCard({ $id, title, featuredimage }) {
+  return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={service.getfilepreview(featuredimage)} alt={title} 
-                className='rounded-xl' />
-
-            </div>
-            <h2
-            className='text-2xl font-bold'
-            >{title}</h2>
+      <div className="postcard w-full bg-gray-100 rounded-xl p-4 duration-300 hover:scale-110">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={service.getfilepreview(featuredimage)}
+            alt={title}
+            className="postcardimg rounded-xl h-44 w-56"
+          />
         </div>
+        <div className="truncate">
+          <h2 className="text-xl font-bold">{title}</h2>
+        </div>
+      </div>
     </Link>
-)
+  );
 }
 
-export default PostCard
+export default PostCard;
